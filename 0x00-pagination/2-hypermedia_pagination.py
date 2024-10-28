67879606be903b85f2a4f87a9375ac6c577115bc
khalidhub7 <louguikhalid7@gmail.com>
2024-10-28 13:18:15 -0700

    hypermedia pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -53,23 +53,22 @@ a database of popular baby names.
                            len(data))]
 
     def get_hyper(
-            self, page: int = 1, page_size: int = 10
+        self, page: int = 1, page_size: int = 10
     ) -> Dict:
         """ hypermedia pagination """
-        data = self.dataset()
-        pagee = self.get_page(page, page_size)
-        page_size = len(pagee)
-        currentpage = page
-        totalpages = math.ceil(
-            len(data) / page_size
-        ) if page_size > 0 else 0
+        data = self.get_page(
+            page, page_size)
+        total_pages = math.ceil(
+            len(self.dataset()) / page_size)
         return {
-            'page_size': page_size,
-            'page': currentpage,
-            'data': pagee,
+            'page_size': len(data),
+            'page': page,
+            'data': data,
             'next_page': page + 1
-            if page < totalpages else None,
+            if page < total_pages
+            else None,
             'prev_page': page - 1
-            if page > 1 else None,
-            'total_pages': totalpages
+            if page > 1
+            else None,
+            'total_pages': total_pages
         }
