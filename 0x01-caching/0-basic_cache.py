d7635ebe3226568d6625cd7d0bc8d5487d3b48fe
khalidhub7 <louguikhalid7@gmail.com>
2024-10-29 09:39:54 -0700

    class BasicCache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-"""Basic dictionary """
-BaseCaching = __import__('base_caching').BaseCaching
+""" caching """
+BaseCaching = __import__(
+    'base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
