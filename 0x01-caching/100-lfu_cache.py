d8c6f8f342fc6bb00105e3294f6a88ec4ef11d7e
khalidhub7 <louguikhalid7@gmail.com>
2024-10-30 07:10:47 -0700

    LFU Caching

---
+++ 0x01-caching/100-lfu_cache.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+""" LFU caching """
+from collections import Counter, OrderedDict
+BaseCaching = __import__(
+    'base_caching').BaseCaching
+
+
+class LFUCache(BaseCaching):
+    """
+caching system without limit
+    """
+
+    def __init__(self):
+        """
+initialize LFU Cache
+        """
+        super().__init__()
+        self.most_used = Counter()
+        self.usage_order = OrderedDict()
+
+    def put(self, key, item):
+        """
+add an item to the cache using LFU caching
+        """
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                self.cache_data[key] = item
+                self.most_used[key] += 1
+            else:
+                if len(self.cache_data
+                       ) >= BaseCaching.MAX_ITEMS:
+                    least_used_key = min(
+                        self.most_used,
+                        key=self.most_used.get)
+                    self.cache_data.pop(
+                        least_used_key)
+                    self.most_used.pop(
+                        least_used_key)
+                    print("DISCARD: {}".format(
+                        least_used_key))
+                self.cache_data[key] = item
+                self.most_used[key] = 1
+
+            self.usage_order[key] = self.most_used[key]
+            self.usage_order.move_to_end(key)
+
+    def get(self, key):
+        """
+get an item by key
+        """
+        if key is not None and key in self.cache_data:
+            self.most_used[key] += 1
+            self.usage_order[key] += 1
+            self.usage_order.move_to_end(key)
+            return self.cache_data[key]
+        return None
