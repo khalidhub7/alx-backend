1e288c4564dff2f7b1d4af479b32bede15e90e92
khalidhub7 <louguikhalid7@gmail.com>
2024-11-05 08:59:49 -0800

    get locale from request

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -17,15 +17,15 @@ app.config.from_object(Config)
 babel = Babel(app=app)
 
 
-@app.route('/', strict_slashes=False)
-def home():
-    """ single route """
-    return render_template('1-index.html')
-
-
 @babel.localeselector
 def get_locale():
     """ determine the best match
     with our supported languages """
     return request.accept_languages.best_match(
         app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """ single route """
+    return render_template('2-index.html')
