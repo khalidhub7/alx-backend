7feb653d179981321c926204c1d4c1bfe969704a
khalidhub7 <louguikhalid7@gmail.com>
2024-11-05 07:45:32 -0800

    basic flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def home():
     """ single route """
     return render_template('0-index.html')
