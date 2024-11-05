6c1b36b387dce89bf09a40b1378db05f84e01895
khalidhub7 <louguikhalid7@gmail.com>
2024-11-05 07:39:59 -0800

    basic flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" basic flask app """
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    """ single route """
+    return render_template('0-index.html')
