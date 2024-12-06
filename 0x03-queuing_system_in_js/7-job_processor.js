1bd97dc6ff05b8be5cf36cc5ae574b6940ee21d5
khalidhub7 <louguikhalid7@gmail.com>
2024-12-06 00:57:47 -0800

    track progress and errors with Kue

--- 0x03-queuing_system_in_js/7-job_processor.js
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -1,22 +1,25 @@
-import { createQueue } from 'kue';
-
-const queue = createQueue();
+import kue from 'kue';
 
 const blacklisted = ['4153518780', '4153518781'];
 
-function sendNotification(phoneNumber, message, job, done) {
+function sendNotification (
+  phoneNumber, message, job, done) {
   job.progress(0, 100);
   if (blacklisted.includes(phoneNumber)) {
-    done(Error(`Phone number ${phoneNumber} is blacklisted`));
-    return;
+    return done(new Error(
+      `Phone number ${phoneNumber} is blacklisted`));
   }
   job.progress(50, 100);
   console.log(
-    `Sending notification to ${phoneNumber}, with message: ${message}`
-  );
+        `Sending notification to ${phoneNumber}, \
+with message: ${message}`);
   done();
 }
 
-queue.process('push_notification_code_2', 2, (job, done) => {
-  sendNotification(job.data.phoneNumber, job.data.message, job, done);
-});
+const queue = kue.createQueue();
+queue.process(
+  'push_notification_code_2', 2, (job, done) => {
+    sendNotification(
+      job.data.phoneNumber, job.data.message, job, done
+    );
+  });
