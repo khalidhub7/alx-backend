cb6357c517d1c8bca088b9329d6524c73c81094c
khalidhub7 <louguikhalid7@gmail.com>
2024-12-07 05:09:23 -0800

    test for job creation

--- 0x03-queuing_system_in_js/8-job.test.js
+++ 0x03-queuing_system_in_js/8-job.test.js
@@ -1,50 +1,93 @@
-import { createQueue } from 'kue';
+import kue from 'kue';
+import { expect } from 'chai';
 import createPushNotificationsJobs from './8-job';
 
 describe('createPushNotificationsJobs', () => {
   let queue;
-
-  beforeEach(() => {
-    queue = createQueue();
+  before(() => {
+    queue = kue.createQueue();
     queue.testMode.enter();
   });
 
   afterEach(() => {
     queue.testMode.clear();
+  });
+
+  after(() => {
     queue.testMode.exit();
   });
 
-  it('should add jobs to the queue', () => {
-    const list = [
-      {
-        phoneNumber: '4153518780',
-        message: 'This is the code 1234 to verify your account',
-      },
-      // Add more jobs to the list as needed
-    ];
-
-    createPushNotificationsJobs(list, queue);
-
-    expect(queue.testMode.jobs.length).toBe(list.length);
-
-    list.forEach((job, index) => {
-      expect(queue.testMode.jobs[index].type).toBe('push_notification_code_3');
-      expect(queue.testMode.jobs[index].data).toEqual(job);
-    });
-
-    queue.testMode.jobs.forEach((job) => {
-      job.on('complete', () => {
-        expect(job.state()).toBe('complete');
-      });
-      job.on('failed', (err) => {
-        expect(job.state()).toBe('failed');
-        expect(err).toBe('Error: Job failed');
-      });
-      job.on('progress', (progress) => {
-        expect(progress).toBe(100);
+  it(
+    'should throw an error if jobs is not an array', () => {
+      expect(() => {
+        createPushNotificationsJobs({}, queue).to.throw(
+          'Jobs is not an array');
       });
     });
-  });
 
-  // Add more test cases as needed
+  it(
+    'should add jobs to the queue correctly', () => {
+      const jobs = [
+        {
+          phoneNumber: '4153518780',
+          message: 'This is the code 1234 to verify your account'
+        },
+        {
+          phoneNumber: '4153518781',
+          message: 'This is the code 4562 to verify your account'
+        },
+        {
+          phoneNumber: '4153518743',
+          message: 'This is the code 4321 to verify your account'
+        },
+        {
+          phoneNumber: '4153538781',
+          message: 'This is the code 4562 to verify your account'
+        },
+        {
+          phoneNumber: '4153118782',
+          message: 'This is the code 4321 to verify your account'
+        },
+        {
+          phoneNumber: '4153718781',
+          message: 'This is the code 4562 to verify your account'
+        },
+        {
+          phoneNumber: '4159518782',
+          message: 'This is the code 4321 to verify your account'
+        },
+        {
+          phoneNumber: '4158718781',
+          message: 'This is the code 4562 to verify your account'
+        },
+        {
+          phoneNumber: '4153818782',
+          message: 'This is the code 4321 to verify your account'
+        },
+        {
+          phoneNumber: '4154318781',
+          message: 'This is the code 4562 to verify your account'
+        },
+        {
+          phoneNumber: '4151218782',
+          message: 'This is the code 4321 to verify your account'
+        }
+      ];
+
+      createPushNotificationsJobs(jobs, queue);
+
+      expect(
+        queue.testMode.jobs.length).to.equal(jobs.length);
+
+      let index = 0;
+      for (const testjob of queue.testMode.jobs) {
+        expect(
+          testjob.type).to.equal('push_notification_code_3');
+        expect(
+          testjob.data.phoneNumber).to.equal(jobs[index].phoneNumber);
+        expect(
+          testjob.data.message).to.equal(jobs[index].message);
+        index = index + 1;
+      }
+    });
 });
