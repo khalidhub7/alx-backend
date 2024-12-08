105b39e4fced5cd801003bd0c19c6bf2a971250b
khalidhub7 <louguikhalid7@gmail.com>
2024-12-08 04:20:36 -0800

    stock reservation and availability

--- 0x03-queuing_system_in_js/9-stock.js
+++ 0x03-queuing_system_in_js/9-stock.js
@@ -1,80 +1,131 @@
+import Redis from 'ioredis';
 import express from 'express';
-import { createClient } from 'redis';
-import { promisify } from 'util';
 
 const app = express();
-const port = 1245;
+const redis = new Redis();
 
-const client = createClient();
+(async () => {
+  await redis.flushall();
+})();
 
-client.on('connect', function () {
-  console.log('Redis client connected to the server');
-});
-
-client.on('error', function (err) {
-  console.log(`Redis client not connected to the server: ${err}`);
-});
-
+// data
 const listProducts = [
-  { id: 1, name: 'Suitcase 250', price: 50, stock: 4 },
-  { id: 2, name: 'Suitcase 450', price: 100, stock: 10 },
-  { id: 3, name: 'Suitcase 650', price: 350, stock: 2 },
-  { id: 4, name: 'Suitcase 1050', price: 550, stock: 5 },
+  {
+    itemId: 1,
+    itemName: 'Suitcase 250',
+    price: 50,
+    initialAvailableQuantity: 4
+  },
+  {
+    itemId: 2,
+    itemName: 'Suitcase 450',
+    price: 100,
+    initialAvailableQuantity: 10
+  },
+  {
+    itemId: 3,
+    itemName: 'Suitcase 650',
+    price: 350,
+    initialAvailableQuantity: 2
+  },
+  {
+    itemId: 4,
+    itemName: 'Suitcase 1050',
+    price: 550,
+    initialAvailableQuantity: 5
+  }
 ];
 
-function getItemById(id) {
-  return listProducts.find((product) => product.id === id);
+// find product by its ID
+function getItemById (itemId) {
+  for (const product of listProducts) {
+    if (product.itemId === itemId) {
+      return product;
+    }
+  }
 }
 
-function reserveStockById(itemId, stock) {
-  client.set(itemId, stock);
+// reserve stock for a product
+async function reserveStockById (itemId, stock) {
+  const key = `item.${itemId}`;
+  try {
+    await redis.set(key, stock);
+  } catch (err) {
+    throw new Error(
+        `Error reserving stock: ${err}`);
+  }
 }
 
-async function getCurrentReservedStockById(itemid) {
-  const getAsync = promisify(client.get).bind(client);
-  const reservedStock = await getAsync(itemid);
-  return reservedStock;
+// check the reserved stock for a product
+async function getCurrentReservedStockById (itemId) {
+  const stock = await redis.get(`item.${itemId}`);
+  return stock ? Number(stock) : null;
 }
 
-app.get('/list_products', (req, res) => {
-  res.json(listProducts);
+app.get('/list_products', (request, response) => {
+  response.statusCode = 200;
+  response.setHeader(
+    'Content-Type', 'application/json');
+  response.json(listProducts);
 });
 
-app.get('/list_products/:itemId', async (req, res) => {
-  const itemId = parseInt(req.params.itemId, 10);
-  const item = getItemById(itemId);
-  if (item === undefined) {
-    res.status(404).json({ status: 'Product not found' });
-  } else {
-    const reservedStock = await getCurrentReservedStockById(itemId);
-    if (reservedStock === null) {
-      item.reservedStock = 0;
-    } else {
-      item.reservedStock = reservedStock;
-    }
-    item.currentQuantity = item.stock - item.reservedStock;
-    res.json(item);
+app.get('/list_products/:itemId', async (request, response) => {
+  const itemId = request.params.itemId;
+  const product = getItemById(Number(itemId));
+
+  // product not found
+  if (!product) {
+    return response.status(404).json(
+      { status: 'Product not found' });
+  }
+
+  // the available stock quantity
+  let currentQuantity = await getCurrentReservedStockById(itemId);
+  if (currentQuantity !== null) {
+    currentQuantity = Number(currentQuantity);
+  }
+  if (currentQuantity === null) {
+    currentQuantity = product.initialAvailableQuantity;
+  }
+
+  // give info about currentQuantity
+  response.status(200).json({
+    ...product,
+    currentQuantity
+  });
+});
+
+app.get('/reserve_product/:itemId', async (request, response) => {
+  const itemId = request.params.itemId;
+  const product = getItemById(Number(itemId));
+
+  // product not found
+  if (!product) {
+    return response.status(404).json(
+      { status: 'Product not found' });
+  }
+
+  // handle available quantity
+  let currentQuantity = await getCurrentReservedStockById(itemId);
+  if (currentQuantity !== null) {
+    currentQuantity = Number(currentQuantity);
+  }
+  if (currentQuantity === null) {
+    currentQuantity = product.initialAvailableQuantity;
+  }
+
+  // handle Reservation
+  if (currentQuantity <= 0) {
+    return response.status(400).json(
+      { status: 'Not enough stock available', itemId });
+  }
+  if (currentQuantity > 0) {
+    await reserveStockById(itemId, currentQuantity - 1);
+    return response.status(200).json(
+      { status: 'Reservation confirmed', itemId });
   }
 });
 
-app.get('/reserve_product/:itemId', async (req, res) => {
-  const itemId = parseInt(req.params.itemId, 10);
-  const item = getItemById(itemId);
-  if (item === undefined) {
-    res.status(404).json({ status: 'Product not found' });
-  } else if (item.stock === 0) {
-    res.status(400).json({ status: 'Not enough stock available', itemId });
-  } else {
-    const reservedStock = await getCurrentReservedStockById(itemId);
-    if (reservedStock === null) {
-      reserveStockById(itemId, 1);
-    } else {
-      reserveStockById(itemId, parseInt(reservedStock, 10) + 1);
-    }
-    res.json({ status: 'Reservation confirmed', itemId: itemId });
-  }
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(1245, 'localhost', () => {
+  console.log('Server is running on http://localhost:1245');
 });
