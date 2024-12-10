28fc412d9f29a8d37b45f20fdc91ab889941e415
khalidhub7 <louguikhalid7@gmail.com>
2024-12-09 17:01:34 -0800

    simple reservation system

--- 0x03-queuing_system_in_js/100-seat.js
+++ 0x03-queuing_system_in_js/100-seat.js
@@ -1,79 +1,95 @@
-import { createClient } from 'redis';
-import { promisify } from 'util';
-import { createQueue } from 'kue';
+import Redis from 'ioredis';
+import kue from 'kue';
 import express from 'express';
 
-const client = createClient();
-
-client.on('connect', function () {
-  console.log('Redis client connected to the server');
-});
-
-client.on('error', function (err) {
-  console.log(`Redis client not connected to the server: ${err}`);
-});
-
-const getAsync = promisify(client.get).bind(client);
-
-let reservationEnabled = true;
-
-const queue = createQueue();
-
+const redis = new Redis();
+const queue = kue.createQueue();
 const app = express();
 
-function reserveSeat(number) {
-  client.set('available_seats', number);
+let reservationEnabled = true;
+async function initializeSeats () {
+  await redis.set('available_seats', 50);
 }
 
-async function getCurrentAvailableSeats() {
-  const reservedStock = await getAsync('available_seats');
-  return reservedStock;
+// retrieves available seats
+async function getCurrentAvailableSeats () {
+  const availableseats = await redis.get('available_seats');
+  return Number(availableseats);
 }
 
+// reserves seats if available
+async function reserveSeat (number) {
+  const currentSeats = await getCurrentAvailableSeats();
+
+  if (currentSeats === 0) {
+    reservationEnabled = false;
+    throw new Error('Not enough seats available');
+  }
+
+  if (currentSeats < number) {
+    throw new Error(
+      'Requested reservation exceeds available seats.');
+  }
+  await redis.set('available_seats', currentSeats - number);
+}
+
+// available seats route
 app.get('/available_seats', async (req, res) => {
   const availableSeats = await getCurrentAvailableSeats();
   res.json({ numberOfAvailableSeats: availableSeats });
 });
 
-app.get('/reserve_seat', async (req, res) => {
-  if (reservationEnabled) {
-    const job = queue.create('reserve_seat').save((err) => {
-      if (!err) {
-        res.json({ status: 'Reservation in process' });
-      } else {
-        res.json({ status: 'Reservation failed' });
-      }
-    });
-    job.on('complete', () => {
-      console.log(`Seat reservation job ${job.id} completed`);
-    });
-    job.on('failed', (errorMessage) => {
-      console.log(`Seat reservation job ${job.id} failed: ${errorMessage}`);
-    });
-  } else {
-    res.json({ status: 'Reservations are blocked' });
+// reserves seats route
+app.get('/reserve_seat', (req, res) => {
+  if (!reservationEnabled) {
+    return res.json({ status: 'Reservation are blocked' });
   }
+
+  const job = queue.create('reserve_seat', { seats: 1 });
+
+  job.save((err) => {
+    if (!err) {
+      return res.json({ status: 'Reservation in process' });
+    }
+    return res.status(500).json(
+      { status: 'Reservation failed', error: err.message });
+  });
+
+  job.on('complete', () => {
+    console.log(`Seat reservation job ${job.id} completed`);
+  });
+
+  job.on('failed', (err) => {
+    console.log(
+      `Seat reservation job ${job.id} failed: ${err.message}`);
+  });
 });
 
+// process route to apply reservation
 app.get('/process', (req, res) => {
   res.json({ status: 'Queue processing' });
+
   queue.process('reserve_seat', async (job, done) => {
-    const availableSeats = await getCurrentAvailableSeats();
-    if (availableSeats > 0) {
-      client.decr('available_seats');
-      const newAvailableSeats = await getCurrentAvailableSeats();
-      if (newAvailableSeats === 0) {
-        reservationEnabled = false;
-      }
+    try {
+      await reserveSeat(job.data.seats);
       done();
-    } else {
-      done(Error('Not enough seats available'));
+    } catch (err) {
+      done(err);
     }
   });
 });
 
-app.listen(1245, () => {
-  console.log('API available on localhost port 1245');
+app.listen(1245, async () => {
+  await initializeSeats();
+  console.log('Server running at http://localhost:1245');
 });
 
-reserveSeat(50);
+/* test
+
+npm run dev 100-seat.js
+curl localhost:1245/available_seats ; echo ""
+curl localhost:1245/reserve_seat ; echo ""
+curl localhost:1245/process ; echo ""
+curl localhost:1245/available_seats ; echo ""
+for n in {1..50}; do curl localhost:1245/reserve_seat ; echo ""; done
+*/
