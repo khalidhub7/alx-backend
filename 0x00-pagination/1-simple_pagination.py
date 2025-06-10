9011772c659bcfbb9c795607b4e2cd1dc11dd988
khalidhub7 <louguikhalid7@gmail.com>
2025-06-10 09:06:34 +0100

    simple pagination get_page() (code refactor)

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -40,6 +40,7 @@ class Server:
     def get_page(
         self, page: int = 1, page_size: int = 10
     ) -> List[List]:
+        """ return data for the page """
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
