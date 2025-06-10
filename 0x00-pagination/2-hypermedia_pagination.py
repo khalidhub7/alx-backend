3ef01938bd5738c95027bf5a11f3ede62aa946c8
khalidhub7 <louguikhalid7@gmail.com>
2025-06-10 20:46:20 +0100

    hypermedia pagination get_hyper() (code refactor)

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,71 +1,68 @@
 #!/usr/bin/env python3
 """ pagination """
 import csv
-from typing import List, Dict
-from math import ceil
+import math
+from typing import Tuple, List, Dict
 
 
-def index_range(
-        page: int, page_size: int):
-    """ start and end index """
-    start = page_size * (page - 1)
-    end = start + page_size
-    return (start, end)
+def index_range(page: int, page_size: int
+                ) -> Tuple[int, int]:
+    """ return start and end index for a page """
+
+    start_index = page_size * (page - 1)
+
+    end_index = start_index + page_size
+
+    return (start_index, end_index)
 
 
 class Server:
-    """Server class to paginate
-a database of popular baby names.
-    """
+    """ server class to paginate baby names database """
+
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
-        """ initializing ... """
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+        """ cached dataset """
+
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
                 dataset = [
-                    row for row in reader]
+                    row for row in reader
+                ]
             self.__dataset = dataset[1:]
+
         return self.__dataset
 
     def get_page(
-            self, page: int = 1, page_size: int = 10
+        self, page: int = 1, page_size: int = 10
     ) -> List[List]:
-        """ get page """
-        assert isinstance(
-            page, int) and page > 0
-        assert isinstance(
-            page_size, int) and page_size > 0
-        pagelen = index_range(
-            page, page_size)
-        data = self.dataset()
-        # the pagelen[0] is page_start not num_of_page
-        if pagelen[0] >= len(data):
-            return []
-        return data[
-            pagelen[0]:min(pagelen[1],
-                           len(data))]
+        """ return data for the page """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        start, end = (index_range(page, page_size))
+        return self.dataset()[start: end]
 
     def get_hyper(
-        self, page: int = 1, page_size: int = 10
+            self, page: int = 1, page_size: int = 10
     ) -> Dict:
         """ hypermedia pagination """
-        data = self.get_page(
-            page, page_size)
-        total_pages = ceil(
-            len(self.dataset()) / page_size)
+        data = self.get_page(page, page_size)
+        total_pages = math.ceil(
+            len(self.__dataset) / page_size
+        )
+        next_page = page + 1 if page < total_pages else None
+        prev_page = page - 1 if page > 1 else None
+
         return {
-            'page_size': len(data), 'page': page,
+            'page_size': len(data),
+            'page': page,
             'data': data,
-            'next_page': page + 1
-            if page < total_pages
-            else None,
-            'prev_page': page - 1
-            if page > 1
-            else None, 'total_pages': total_pages}
+            'next_page': next_page,
+            'prev_page': prev_page,
+            'total_pages': total_pages
+        }
