98ffe342842b8986b923780cbc726d89946bcf7f
khalidhub7 <louguikhalid7@gmail.com>
2025-06-11 03:53:47 +0100

    BasicCache class (code refactor)

--- 0x01-caching/base_caching.py
+++ 0x01-caching/base_caching.py
@@ -1,38 +1,34 @@
 #!/usr/bin/python3
-""" caching """
+""" BaseCaching module """
 
 
 class BaseCaching():
-    """ BaseCaching defines:
-- constants of your caching system
-- where your data are stored (in a dictionary)
+    """
+basecaching defines constants
+and stores data in a dictionary
     """
     MAX_ITEMS = 4
 
     def __init__(self):
-        """ Initiliaze
-        """
+        """ initiliaze """
         self.cache_data = {}
 
     def print_cache(self):
-        """ Print the cache
-        """
+        """ print the cache """
         print("Current cache:")
-        for key in sorted(
-                self.cache_data.keys()):
+        for key in sorted(self.cache_data.keys()):
             print("{}: {}".format(
-                key, self.cache_data.get(key)))
+                key, self.cache_data.get(key)
+            ))
 
     def put(self, key, item):
-        """ Add an item in the cache
-        """
+        """ add item to cache """
         raise NotImplementedError(
-            "put must be implemented i\
-                n your cache class")
+            "put must be implemented in your cache class"
+        )
 
     def get(self, key):
-        """ Get an item by key
-        """
+        """ get item by key """
         raise NotImplementedError(
-            "get must be implemented i\
-                n your cache class")
+            "get must be implemented in your cache class"
+        )
