fff4335a5f809e56a7de8a4b61275eb7fd2d1134
khalidhub7 <louguikhalid7@gmail.com>
2025-06-13 07:47:26 +0100

    basic flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
 """ basic flask app """
+
 from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route(
+    '/', strict_slashes=False, methods=['GET']
+)
 def home():
-    """ single route """
+    """ home """
     return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(
+        host='localhost', port=5000
+    )
