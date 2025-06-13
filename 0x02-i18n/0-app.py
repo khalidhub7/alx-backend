90771f6aa24ef40f36b1a0dbe98e7b836d925c09
khalidhub7 <louguikhalid7@gmail.com>
2025-06-13 08:05:32 +0100

    basic flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ basic flask app """
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
@@ -10,7 +10,7 @@ app = Flask(__name__)
 )
 def home():
     """ home """
-    return "<h1>Welcome to Holberton</h1>"
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
