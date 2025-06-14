52dc9639a04a44e6d713afebe77fb56e9cfbf12f
khalidhub7 <louguikhalid7@gmail.com>
2025-06-14 11:34:54 +0200

    parametrize templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -27,7 +27,7 @@ def get_locale():
 @app.route('/', strict_slashes=False,
            methods=['GET'])
 def home():
-    """ render the home page template """ 
+    """ render the home page template """
     return render_template('3-index.html')
 
 
