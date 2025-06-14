b96880b45ee98b97a509d447058eb95f649ac3be
khalidhub7 <louguikhalid7@gmail.com>
2025-06-14 11:05:15 +0200

    parametrize templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,35 +1,37 @@
 #!/usr/bin/env python3
-""" basic Flask app with Babel setup """
-from flask import (Flask, request,
-                   render_template)
-from flask_babel import Babel
-app = Flask(__name__)
+""" basic flask app """
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
 
 
 class Config:
-    """ config class """
+    """ app config for Babel settings """
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_LOCALE = LANGUAGES[0]
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
+app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
 
 
 @babel.localeselector
 def get_locale():
-    """ determine the best match
-    with our supported languages """
-    return request.accept_languages.best_match(
-        app.config['LANGUAGES'])
+    """ select best matching language """
+    bestMatchLang = request.accept_languages.best_match(
+        Config.LANGUAGES)
+    return bestMatchLang
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/', strict_slashes=False,
+           methods=['GET'])
 def home():
-    """ single route """
-    return render_template('3-index.html')
+    """ home """
+    return render_template('3-index.html',
+                           home_title='Welcome to ALX',
+                           home_header='Hello world!')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='localhost', port=5000)
