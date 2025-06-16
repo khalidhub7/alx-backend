6dbd10d620fe81935704f6770a432f3af13c279c
khalidhub7 <louguikhalid7@gmail.com>
2025-06-16 03:17:06 +0100

    done

--- 0x02-i18n/setup.sh
+++ 0x02-i18n/setup.sh
@@ -1,23 +1,36 @@
-
-
-# install flask_babel==2.0.0
+python3 --version
 pip3 install flask_babel==2.0.0
 
 
-# extract all the translatable messages\
-# from Python and HTML files
-# create (messages.pot file)
-pybabel extract -F babel.cfg -o messages.pot .
+# format html and python files
+prettier --write templates/5-index.html
+autopep8 5-app.py --in-place --aggressive --aggressive
 
 
-# create Translation Files (create .po files)
-pybabel init -i messages.pot -d translations -l en
+# extract msgs, init langs (en, fr)
+pybabel extract -F babel.cfg -o messages.pot . && \
+pybabel init -i messages.pot -d translations -l en && \
 pybabel init -i messages.pot -d translations -l fr
-
-
-# compile Translations(create .mo files)
+# edit .po files, then compile translations
 pybabel compile -d translations
 
 
-# to translate use
+# run app
+python3 3-app.py
 curl -H "Accept-Language: fr" http://127.0.0.1:5000/
+
+
+# babel.cfg for task 3
+: '
+[python: 3-app.py]
+[jinja2: templates/3-index.html]
+extensions=jinja2.ext.autoescape,jinja2.ext.with_
+'
+
+
+# workflow
+: '
+Request → set user → choose locale (for translations) → 
+choose timezone (for time) → format time → 
+render template with translations
+'
