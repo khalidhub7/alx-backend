df34c83ee21a47e67b5877fd7afb060492e3e07c
khalidhub7 <louguikhalid7@gmail.com>
2025-06-21 12:11:00 +0100

    job processor

--- 0x03-queuing_system_in_js/7-job_processor.js
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -1,25 +1,27 @@
-import kue from 'kue';
+import Kue from 'kue';
 
-const blacklisted = ['4153518780', '4153518781'];
+const queue = Kue.createQueue();
 
-function sendNotification (
-  phoneNumber, message, job, done) {
+const blacklist = ['4153518780', '4153518781'];
+
+const sendNotification = (
+  phoneNumber, message, job, done,
+) => {
   job.progress(0, 100);
-  if (blacklisted.includes(phoneNumber)) {
+  if (blacklist.includes(phoneNumber)) {
     return done(new Error(
-      `Phone number ${phoneNumber} is blacklisted`));
+      `Phone number ${phoneNumber} is blacklisted`,
+    ));
   }
   job.progress(50, 100);
   console.log(
-        `Sending notification to ${phoneNumber}, \
-with message: ${message}`);
-  done();
-}
+    `Sending notification to ${phoneNumber}, with message: ${message}`,
+  );
+  return done();
+};
 
-const queue = kue.createQueue();
-queue.process(
-  'push_notification_code_2', 2, (job, done) => {
-    sendNotification(
-      job.data.phoneNumber, job.data.message, job, done
-    );
-  });
+queue.process('push_notification_code_2', 2, (job, done) => {
+  sendNotification(
+    job.data.phoneNumber, job.data.message, job, done,
+  );
+});
